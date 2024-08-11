987f435c9f7389ebed12f8ef2becf55f9479cffc
Crimcat <adriangutcarb@gmail.com>
2024-08-10 22:56:58 -0600

    starting

---
+++ Tercer_Proyecto_2Q_2024/Capa_Datos/CLS_usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tercer_Proyecto_2Q_2024.Capa_Datos
+{
+    public class CLS_usuarios
+    {
+        public static int usuario_id { get; set; }
+        public static string nombre { get; set; }
+        public static string correo { get; set; }
+        public static string contra { get; set; }
+    }
+}
